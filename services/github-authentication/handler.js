9b8123c512e7c4a4ae89556fc328ff03c3ac46a5
Evan Cloutier <12ec49@queensu.ca>
2017-09-03 23:53:52 -0400

    Modularity

--- services/github-authentication/handler.js
+++ services/github-authentication/handler.js
@@ -1,36 +1,8 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-const Authenticator = require('./handlers/authenticationHandler');
+const callbackHandler = require('./handlers/CallbackHandler');
 
-const dynamo = new AWS.DynamoDB.DocumentClient();
-let authenticator = new Authenticator(dynamo);
-
-module.exports.callback = (event, context, callback) => {
-  authenticator.getAccessToken(event.queryStringParameters.code)
-    .then((token) => {
-      authenticator.getUserData(token)
-        .then((user) => {
-          const timestamp = new Date().getTime();
-          const params = {
-            TableName: process.env.DYNAMODB_TABLE,
-            Item: {
-              id: user.id,
-              accessToken: token.access_token,
-              scope: token.scope.split(','),
-              tokenType: token.token_type,
-              createdAt: timestamp,
-              updatedAt: timestamp,
-            },
-          };
-
-          authenticator.save(params, callback);
-        })
-        .catch((error) => {
-          callback(error);
-        });
-    })
-    .catch((error) => {
-      callback(error);
-    })
-};
+module.exports.callback = 
+  (event, context, callback) => 
+    callbackHandler(event, context, callback);
+    
